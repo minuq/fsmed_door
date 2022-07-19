aa66689960a2db43751db1a0346398912f5ad7f6
minuq <linke.florian@gmail.com>
2022-07-20 00:23:35 +0200

    typo

--- ip.py
+++ ip.py
@@ -3,6 +3,6 @@ import slack
 
 try:
     ip = check_output(['hostname', '--all-ip-addresses'])
-    slack.main("mensa.ts","ip: {0}".format(ip.decode("utf-8")))
+    slack.main("ip.ts","ip: {0}".format(ip.decode("utf-8")))
 except Exception as err:
     print(str("error: {0}".format(err)))
